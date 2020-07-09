2c2d1f292f022d114af25dd80459cfcc997806fb
liuyt <numb327@gmail.com>
2020-07-09 15:25:35 +0800

    init

---
+++ Design.Pattern/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace Design.Pattern
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
---
+++ Design.Pattern/Singleton.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Design.Pattern
+{
+    /// <summary>
+    /// 单例模式
+    /// </summary>
+    public sealed class Singleton
+    {
+        private static Singleton _instance = null;
+        private static readonly object _obj = new object();
+
+        private Singleton()
+        {
+        }
+
+        public static Singleton Instance
+        {
+            get
+            {
+                lock (_obj)
+                {
+                    if (_instance == null)
+                    {
+                        _instance = new Singleton();
+                    }
+
+                    return _instance;
+                }
+            }
+        }
+    }
+}
